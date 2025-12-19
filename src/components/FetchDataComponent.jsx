ac138b701eda843901ca7156b581110a47a7b9a7
srighankittu <107845663+srighankittu@users.noreply.github.com>
2025-12-19 06:47:25 +0530

    tailwind init, styling product card

--- src/components/FetchDataComponent.jsx
+++ src/components/FetchDataComponent.jsx
@@ -5,6 +5,7 @@ const FetchDataComponent = () => {
   // [{}, {}]
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [x, setX] = useState(0);
 
   async function fetchProducts() {
     //API
@@ -17,37 +18,36 @@ const FetchDataComponent = () => {
 
   useEffect(() => {
     fetchProducts();
-  }, []);
-
-  function removeItem(id) {
-    //products = [null, {},..] 30
-    //products = [{}, {},..] 29
-    //product = {id:1, title:""};
-
-    let filteredProducts = products.filter((product) => {
-      if (product.id !== id) return product;
-    });
-
-    // filteredProducts
-    setProducts(filteredProducts);
-  }
+  }, [x]);
 
   if (loading) return <div>loading...</div>;
 
   return (
     <>
-      <h1>Products</h1>
-      {products.map((product) => {
-        return (
-          <Product
-            key={product.id} //default
-            title={product.title}
-            description={product.description}
-            price={product.price}
-            image={product.images[0]}
-          />
-        );
-      })}
+      <p className="text-5xl">Products</p>
+      <div className="border-solid border-2 w-[245px]">
+        <input
+          className="border-solid border-1"
+          type="text"
+          placeholder="Search.."
+        ></input>
+        <button>Search</button>
+      </div>
+      <div className="flex flex-wrap">
+        {products.map((product) => {
+          return (
+            <>
+              <Product
+                key={product.id} //default
+                title={product.title}
+                description={product.description}
+                price={product.price}
+                image={product.images[0]}
+              />
+            </>
+          );
+        })}
+      </div>
     </>
   );
 };
@@ -56,4 +56,4 @@ export default FetchDataComponent;
 
 // virtual DOM - copy of the real dom.
 // vd 1 - vd
-2;
+//2;
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 const Product = ({ title, description, price, image }) => {
   return (
-    <div>
-      {title}-{price}-{description}
-      <img src={image}></img>
+    <div className="w-80 m-2 border-solid border-4 flex">
+      <div>
+        <img src={image}></img>
+      </div>
+      <div>
+        <div>Name: {title}</div>
+        <div>Description: {description}</div>
+        <div>Price: {price}</div>
+      </div>
     </div>
   );
 };
